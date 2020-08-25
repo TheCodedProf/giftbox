00ffc31eab78964ec9dd21511c5e88905390f52a
dragdev <samueljohnnicholson@gmail.com>
2020-08-25 20:28:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# GiftBox
+
+Giftbox is just a test project to see how our new developers adapt to how others code, and also as a training tool on how to write discord.py.
+
+You can use this in your servers, or create your own copy by downloading it.
+
+Its a giveaway bot.
---
+++ bot/bot.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created w/ "python3 -m discord newbot"
+
+from discord.ext import commands
+import sys
+from bot import config
+
+
+class Bot(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix=commands.when_mentioned_or(':gift:', '\U0001f381'), **kwargs)
+        for cog in config.cogs:
+            try:
+                self.load_extension(cog)
+            except Exception as exc:
+                print('Could not load extension {0} due to {1.__class__.__name__}: {1}'.format(cog, exc))
+
+    async def on_ready(self):
+        print('Logged on as {0} (ID: {0.id})'.format(self.user))
+
+
+bot = Bot()
+
+@bot.event
+async def on_connect():
+    print(f"Connected to discord.")
+
+@bot.event
+def on_disconnect():
+    print(f"Bot disconnected from discord!", file=sys.stderr)
+
+# write general commands here
+
+bot.run(config.token)
