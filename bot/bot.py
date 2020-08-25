88df6ce7ed79ab5a8560d38d2771a9b18d964202
Zander Picon <zanderpicon@Zanders-MacBook-Pro.local>
2020-08-25 18:36:10 -0400

    Fixed typo and added ping command bot.command() is not capitalized Ping!

--- bot/bot.py
+++ bot/bot.py
@@ -32,16 +32,20 @@ def on_disconnect():
 
 # write general commands here
 
-@bot.Command()
+@bot.command()
 async def info(ctx):
     pass
 
-@bot.Command()
+@bot.command()
 async def about(ctx):
     pass
 
-@bot.Command()
+@bot.command()
 async def invite(ctx):
     await ctx.send(f"**Invite me to your server!** \n[link]")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! `{bot.latency*1000}ms`")
 
 bot.run(config.token)
